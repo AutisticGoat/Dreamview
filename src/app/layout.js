500c85b263b3fa8f79bff8053eaeca22014674f6
AutisticGoat <83523192+AutisticGoat@users.noreply.github.com>
2026-07-28 17:51:13 -0700

    Revert "Añadidos orbes reaccionarios al scroll"
    This reverts commit c5d5de57121590c8bffceca41621e8f19ced56b9.

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,6 @@ import './globals.css'
 import SessionProvider from '@/components/SessionProvider'
 import ParticlesBackground from '@/components/ParticlesBackground'
 import AnimatedLayout from '@/components/AnimatedLayout'
-import ScrollOrbs from '@/components/ScrollOrbs'
 
 const outfit = Outfit({
   subsets:  ['latin'],
@@ -22,7 +21,6 @@ export default function RootLayout({ children }) {
       <body className={outfit.variable}>
         <SessionProvider>
           <ParticlesBackground />
-          <ScrollOrbs />
           <div style={{ position: 'relative', zIndex: 1 }}>
             <AnimatedLayout>
               {children}
